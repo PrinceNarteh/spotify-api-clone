48e4e4fd66a97caab958f7a57b512ff657577e4e
Prince Narteh <prinarteh@gmail.com>
2024-04-30 17:54:53 -0700

    feat: add middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,31 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from 'songs/songs.module';
 import { CommonModule } from './common/common.module';
+import { LoggerMiddleware } from 'common/middlewares/logger.middleware';
+import { SongsController } from 'songs/songs.controller';
 
 @Module({
   imports: [SongsModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // option 1
+    // consumer.apply(LoggerMiddleware).forRoutes('songs');
+    //
+    // option 2
+    // consumer.apply(LoggerMiddleware).forRoutes({
+    //   path: 'songs',
+    //   method: RequestMethod.POST,
+    // });
+    //
+    // option 3
+    consumer.apply(LoggerMiddleware).forRoutes(SongsController);
+
+    // Exclude routes from middleware
+    // consumer.apply(LoggerMiddleware).exclude('/login', 'register');
+  }
+}
