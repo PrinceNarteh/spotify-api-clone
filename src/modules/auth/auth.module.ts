812f2618f786d63510049905a4f149476a6eae72
Prince Narteh <prinarteh@gmail.com>
2024-05-26 05:30:19 -0700

    feat: add JWT strategy

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -6,12 +6,18 @@ import { HashingService } from './hashing/hashing.service';
 import { BcryptService } from './hashing/bcrypt.service';
 import { JwtModule } from '@nestjs/jwt';
 import { authConstants } from './auth.constants';
+import { PassportModule } from '@nestjs/passport';
+import { JWTStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
     JwtModule.register({
       secret: authConstants.secret,
+      signOptions: {
+        expiresIn: '1d',
+      },
     }),
+    PassportModule,
     UsersModule,
   ],
   controllers: [AuthController],
@@ -21,6 +27,7 @@ import { authConstants } from './auth.constants';
       useClass: BcryptService,
     },
     AuthService,
+    JWTStrategy,
   ],
 })
 export class AuthModule {}
---
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { authConstants } from 'auth/auth.constants';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JWTStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secret: authConstants.secret,
+      ignoreExpiration: false,
+    });
+  }
+
+  async validate(payload: any) {
+    return { userId: payload.sub, email: payload.email };
+  }
+}
