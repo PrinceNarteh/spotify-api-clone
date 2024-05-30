bc9ca36dd37f3e2dcc0d79f3e95a8afccb0c0901
Prince Narteh <prinarteh@gmail.com>
2024-05-30 06:38:14 -0700

    feat: added enable-2fa handler

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'users/dto/create-user.dto';
 import { LoginDto } from './dto/loginDto';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { Enable2FAType } from 'types/auth-types';
 
 @Controller('auth')
 export class AuthController {
@@ -15,5 +17,11 @@ export class AuthController {
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
+  }
+
+  @Post('enable-2fa')
+  @UseGuards(JwtAuthGuard)
+  async enable2FA(@Request() req): Promise<Enable2FAType> {
+    return this.authService.enable2FA(req.user.id);
   }
 }
